315dcf07aed994b96dbf0b2caa84c627f3e8bfe9
Masika <masikaomondi@gmail.com>
2022-09-26 17:44:49 +0300

    Completed Assignment

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+
+  const navTags = links.map(link => <a href ={`#${link}`} key={link}>{link}</a>)
+
+  return <nav>{navTags}</nav>;
+}
+
+export default NavBar;
